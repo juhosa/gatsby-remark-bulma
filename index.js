f0405afcab65a853ee56467f8f19339ac4e7420e
Juho Salli <juho.salli@gmail.com>
2019-06-05 22:11:53 +0300

    Blockquotes work naively (all links and stuff get stripped away)

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const visit = require("unist-util-visit");
-const toString = require("mdast-util-to-string");
+const visit = require("unist-util-visit")
+const toString = require("mdast-util-to-string")
 
 module.exports = ({ markdownAST }, pluginOptions) => {
   // Manipulate AST
   visit(markdownAST, "heading", node => {
     // console.log(node)
     let { depth } = node
-    console.log({depth})
+    // console.log({ depth }, "syvyys")
 
     let text = toString(node)
 
@@ -14,6 +14,25 @@ module.exports = ({ markdownAST }, pluginOptions) => {
         <h${depth} class="title is-${depth}">
           ${text}
         </h${depth}>
+      `
+
+    node.type = "html"
+    node.children = undefined
+    node.value = html
+  })
+
+  visit(markdownAST, "blockquote", node => {
+    // console.log(node)
+    let text = toString(node)
+    // console.log({ text })
+
+    // '<div class="content"><blockquote>$1</blockquote></div>',
+    const html = `
+        <div class="content">
+          <blockquote>
+            ${text}
+          </blockquote>
+        </div>
       `
 
     node.type = "html"
