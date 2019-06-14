32d46b6fca16855402a7581546395dfd88e805f9
Juho Salli <juho.salli@gmail.com>
2019-06-14 23:15:20 +0300

    Lists working

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 const visit = require("unist-util-visit-parents")
-const toString = require("mdast-util-to-string")
 
 module.exports = ({ markdownAST }, pluginOptions) => {
-  // Manipulate AST
   visit(markdownAST, "heading", (node, ancestors) => {
-    // console.log(node)
     let { depth } = node
-    // console.log({ depth }, "syvyys")
     node.data = {
       hProperties: { class: `title is-${depth}` },
     }
   })
 
   visit(markdownAST, "blockquote", (node, ancestors) => {
+    const div = {
+      type: "section",
+      data: {
+        hName: "div",
+        hProperties: { class: "content" },
+      },
+      children: [node],
+    }
+    const parent = ancestors[ancestors.length - 1]
+    const startIndex = parent.children.indexOf(node)
+    parent.children.splice(startIndex, 1, div)
+  })
+
+  visit(markdownAST, "list", (node, ancestors) => {
     const div = {
       type: "section",
       data: {
