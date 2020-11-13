9f0efb4182d152cdbdcad30506e1e7e2aacfdfe9
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2020-11-13 23:10:13 +1100

    simple table tag

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const visit = require("unist-util-visit-parents")
+const visit = require("unist-util-visit-parents");
 
 module.exports = ({ markdownAST }, pluginOptions) => {
   visit(markdownAST, "heading", (node, ancestors) => {
-    let { depth } = node
+    let { depth } = node;
     node.data = {
       hProperties: { class: `title is-${depth}` },
-    }
-  })
+    };
+  });
 
   visit(markdownAST, "blockquote", (node, ancestors) => {
     const div = {
@@ -16,11 +16,11 @@ module.exports = ({ markdownAST }, pluginOptions) => {
         hProperties: { class: "content" },
       },
       children: [node],
-    }
-    const parent = ancestors[ancestors.length - 1]
-    const startIndex = parent.children.indexOf(node)
-    parent.children.splice(startIndex, 1, div)
-  })
+    };
+    const parent = ancestors[ancestors.length - 1];
+    const startIndex = parent.children.indexOf(node);
+    parent.children.splice(startIndex, 1, div);
+  });
 
   visit(markdownAST, "list", (node, ancestors) => {
     const div = {
@@ -30,19 +30,25 @@ module.exports = ({ markdownAST }, pluginOptions) => {
         hProperties: { class: "content" },
       },
       children: [node],
-    }
-    const parent = ancestors[ancestors.length - 1]
-    const startIndex = parent.children.indexOf(node)
-    parent.children.splice(startIndex, 1, div)
-  })
+    };
+    const parent = ancestors[ancestors.length - 1];
+    const startIndex = parent.children.indexOf(node);
+    parent.children.splice(startIndex, 1, div);
+  });
 
   visit(markdownAST, "paragraph", (node, ancestors) => {
     if (ancestors[ancestors.length - 1].type === "root") {
       node.data = {
         hProperties: { class: "content" },
-      }
+      };
     }
-  })
+  });
 
-  return markdownAST
-}
+  visit(markdownAST, "table", (node, ancestors) => {
+    node.data = {
+      hProperties: { class: `table` },
+    };
+  });
+
+  return markdownAST;
+};
